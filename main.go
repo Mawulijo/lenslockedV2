573535e93c380817d79563d7fdbe69c7aab1d6d4
Joshua Agbeku <mawulidickson@gmail.com>
2023-12-30 05:34:32 +0000

    handling errors for templates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 
@@ -14,11 +15,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "home.gohtml")
 	tpl, err := template.ParseFiles(tplPath)
 	if err != nil {
-		panic(err) //TODO: Remove the panic
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
 	}
 	err = tpl.Execute(w, nil)
 	if err != nil {
-		panic(err) //TODO: Remove the panic
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
 	}
 }
 func contactHandler(w http.ResponseWriter, r *http.Request) {
